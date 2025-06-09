5f2d3ece8b334259a40bd515f111466220276351
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-06-09 22:59:31 +0600

    importance of functions

---
+++ first_program/imp_func.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to the application.")
+
+	var name string
+	fmt.Println("Enter your name - ")
+	fmt.Scanln(&name)
+
+	var num1 int
+	var num2 int
+	fmt.Println("Enter first number - ")
+	fmt.Scanln(&num1)
+
+	fmt.Println("Enter second number - ")
+	fmt.Scanln(&num2)
+
+	sum := num1 + num2
+
+	fmt.Println("Hello,", name)
+	fmt.Println("Summation = ", sum)
+}
